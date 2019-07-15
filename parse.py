cb21f66e8b07462bfb04f86e0becb2d2bf3b3d43
Misiu Pajor <misiu.pajor@datadoghq.com>
2019-07-15 09:23:33 +0200

    Removed need of local html dependencies.

--- parse.py
+++ parse.py
@@ -1,28 +1,23 @@
 import re
+import requests
 
-''' html source from: https://www.dynatrace.com/technologies/ '''
 def get_dynatrace_integrations():
-    with open('./dt_integrations.html') as integrations_file:
-        integrations_text = integrations_file.read()
+    r = requests.get("https://www.dynatrace.com/technologies/")
+    DT_INTS = re.findall('symbol id=\"(.*?)\"', r.text)
 
-    DT_INTS = re.findall('symbol id=\"(.*?)\"', integrations_text)
     return DT_INTS
 
-''' html source from: https://www.signalfx.com/integrations/ '''
 def get_signalfx_integrations():
-    with open('./sfx_integrations.html') as integrations_file:
-        integrations_text = integrations_file.read()
-
-    results = re.findall('<h2 class=\"h2-2\">(.*?)</h2>', integrations_text)
+    r = requests.get("https://www.signalfx.com/integrations/")
+    results = re.findall('<h2 class=\"h2-2\">(.*?)</h2>', r.text)
     SFX_INTS = [x for x in results if "Signal" not in x]
 
     return SFX_INTS
 
-''' html source from: https://docs.datadoghq.com/integrations '''
 def get_datadog_integrations():
-    with open('./dd_integrations.html') as integrations_file:
-        integrations_text = integrations_file.read()
-    DD_INTS = re.findall('<h4 class=\"card-title\">(.*?)</h4>', integrations_text)
+    r = requests.get("https://docs.datadoghq.com/integrations/")
+    DD_INTS = re.findall('<a class=card-img href=https://docs.datadoghq.com/integrations/(.*?)>', r.text)
+
     return DD_INTS
 
 DT_INTS = get_dynatrace_integrations()
